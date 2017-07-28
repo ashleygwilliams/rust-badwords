df4b0396847a5bc9b02256dd55f4fdbec4505231
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2017-07-28 17:31:15 -0400

    Use `includue_str!` to return string slices with the static lifetime

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-use std::fs::File;
-use std::io::prelude::*;
+pub fn list() -> Vec<&'static str> {
+    let contents = include_str!("words.txt");
 
-pub fn list() -> Vec<String> {
-    let mut file = File::open("./words.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-
-    let words: Vec<String> = contents
+    let words: Vec<&'static str> = contents
         .split('\n')
-        .map(|x| x.to_string())
         .collect();
 
     words
